87f6331c26b10a76a0362b408b7e19cf247040e6
Sophie Dubois <sophiemargueritedubois@gmail.com>
2020-01-08 17:16:05 -0800

    All dynamic layout styles complete

--- generateBoard.js
+++ generateBoard.js
@@ -1,6 +1,19 @@
+const WIDTH = 498;
+const HEIGHT = 498;
+
+let layouts = {
+    1:'"a"',
+    2:'"a" "b"',
+    3:'"a b c" "d e f" "g h i"',
+    4:'"a b c d" "e f g h" "i j k l" "m n o p"',
+    5:'"a b c d e" "f g h i j" "1k l m n o" "p q r s t" "u v w x y"'
+}
+
+// '"0 1 2" "3 4 5" "6 7 8"',
 //Generate board
 let [gameBoard, blankTile] = generateBoard(3);
-// adjustStyles(n);
+adjustStyles(3);
+createTiles(gameBoard);
 
 //Generates a board of size n^2
 //Generates the board and stores it in Gameboard.
@@ -23,11 +36,28 @@ function adjustStyles(n) {
     document.querySelector('.container').style.gridTemplateAreas = layouts[n];
 }
 
-let layouts = {
-    1:'"1"',
-    2:'"1" "2"',
-    3:'"one two tree" "four five six" "seven eight nine"',
-    4:'"0 1 2 3" "4 5 6 7" "8 9 10 11" "12 13 14 15"',
-    5:'"0 1 2 3 4" "5 6 7 8 9" "10 11 12 13 14" "15 16 17 18 19" "20 21 22 23 24"'
+function createTiles(board) {
+    document.documentElement.style.setProperty("--tileHeight", `${Math.floor(HEIGHT/board.length)}px`);
+    document.documentElement.style.setProperty("--tileWidth", `${Math.floor(WIDTH/board[0].length)}px`);
+    let parent = document.querySelector('.container');
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            let newTile = document.createElement('div');
+            let pos = (i * board.length + j);
+            newTile.id = pos;
+            newTile.dataset.pos = pos;
+            newTile.classList.add('tile');
+            newTile.style.backgroundPosition = `${-j * Math.floor(WIDTH/board.length)}px ${-i * Math.floor(HEIGHT/board[0].length)}px`;
+            (i === board.length -1 && j === board[0].length - 1) ? newTile.style.zIndex = 0 : newTile.style.zIndex = 1;
+            newTile.style.gridArea = String.fromCharCode(pos + 97);
+            
+            parent.append(newTile);   
+        }
+    }
+   
 }
-// var template3 = '"0 1 2" "3 4 5" "6 7 8"';
+
+let tiles = document.querySelectorAll('.tile');
+
+
+
